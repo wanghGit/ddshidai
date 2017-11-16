329d87dcf79cdfd4c7ce915899c43f89b1a187df
lqh <blankhrt0629@gmail.com>
2017-11-16 18:34:36 +0800

    login logout

--- src/main/java/com/ddshidai/dao/UserMapper.java
+++ src/main/java/com/ddshidai/dao/UserMapper.java
@@ -18,4 +18,6 @@ public interface UserMapper {
     int updateByPrimaryKey(User record);
 
     List<User> queryAll();
+
+    User login(String phone, String password);
 }
--- src/main/java/com/ddshidai/service/UserService.java
+++ src/main/java/com/ddshidai/service/UserService.java
@@ -5,20 +5,30 @@ import org.springframework.stereotype.Service;
 
 import com.ddshidai.dao.UserMapper;
 import com.ddshidai.model.User;
+
 /**
- * 
  * @author wangheng
- *
  */
 @Service
 public class UserService {
-	@Autowired
-	UserMapper userMapper;
-	public User selectByPrimaryKey(int id) {
-		return userMapper.selectByPrimaryKey(id);
-	};
+    @Autowired
+    UserMapper userMapper;
 
-	public Object queryAll(int id) {
-		return  userMapper.selectByPrimaryKey(id);
-	}
+    public User selectByPrimaryKey(int id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+    ;
+
+    public Object queryAll(int id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+    public User login(String phone, String password) {
+        return userMapper.login(phone, password);
+    }
+
+    public int register(User user) {
+        return userMapper.insertSelective(user);
+    }
 }
